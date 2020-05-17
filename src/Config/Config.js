b9c192c644a615319c233f10134e627ef599bf49
Emre Altın <iletisim@emrealtin.com.tr>
2020-05-17 19:53:15 +0300

    update

---
+++ src/Config/Config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyANcgh8kH-IpMwIGHkQzECI4sZ1fVuGGmQ",
+    authDomain: "react-todo-49809.firebaseapp.com",
+    databaseURL: "https://react-todo-49809.firebaseio.com",
+    projectId: "react-todo-49809",
+    storageBucket: "react-todo-49809.appspot.com",
+    messagingSenderId: "975862991274",
+    appId: "1:975862991274:web:3e494bccb382ca7c24c3d3"
+};
---
+++ src/components/NoteForm.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class NoteForm extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            netNoteTitle : "",
+            newNoteContent : ""
+        }
+
+        this.writeNote = this.writeNote.bind(this);
+    }
+
+    writeNote(){
+
+        var data = {
+            noteTitle : this.state.netNoteTitle,
+            noteContent : this.state.newNoteContent
+        }
+
+        this.props.addNote(data);
+
+        this.setState({
+            newNoteTitle : "",
+        });
+    }
+
+    render() {
+        return (
+            <div className="pt-callout .modifier cardCustomCss">
+                <h4 className="pt-callout-title">Add New Note</h4>
+                <input placeholder="Note Title"
+                       className="pt-input .modifier pt-intent-success inputCustomCss"
+                       dir="auto"
+                       value={this.state.newNoteTitle}
+                       onChange={event => this.setState({newNoteTitle: event.target.value})}
+                />
+                <input placeholder="Note Content"
+                       className="pt-input .modifier pt-intent-success inputCustomCss"
+                       dir="auto"
+                       value={this.state.newNoteContent}
+                       onChange={event => this.setState({newNoteContent: event.target.value})}/>
+
+                <button
+                    type="button"
+                    className="pt-button pt-intent-success"
+                    onClick={this.writeNote} >
+                    Add Note
+                    <span className="pt-icon-standard pt-icon-arrow-right pt-align-right"></span>
+                </button>
+            </div>
+        );
+    }
+}
+
+export default NoteForm;
